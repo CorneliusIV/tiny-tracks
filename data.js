c62c471acdfbcbf42c249f4047b085968b56eb82
cornelius <cornelius@isl.co>
2019-05-14 21:12:16 -0400

    typo

--- data.js
+++ data.js
@@ -9,8 +9,6 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: 'https://koolexposure.com'
 })
 
-const listOArist = new Set(['naia izumi', 'tank and the bangas', 'gaelynn lea'])
-
 const youtube = google.youtube({
   version: 'v3',
   auth: process.env.YOUTUBE_API_KEY
@@ -19,19 +17,19 @@ const youtube = google.youtube({
 // Get artist's name from Youtube title and normalize it
 function getTitle(item) {
   const videoTitle = item.snippet.title
-  // The titles normally follow on of the two formarts
+  // The titles normally follow one of the two formats
   // Artist Name : Description
   // Artist Name - Description
   // Artist Name (Description)
   // Artist Name NPR Descrition
-  // Let's break the title into two parts at one of the special charaters
+  // Let's break the title into two parts at one of the special characters
   const artistString = videoTitle.split(/[-(:\/-]/)[0]
   // Sorry NPR but this if you are included in the Title split there as well
   const artist = artistString.split('NPR')[0]
   // Strip white space and set to lower
   const artistName = artist.replace(/^\s+|\s+$/g, '').toLowerCase()
   // TODO: This still still kind of hacky and doesn't cover all cases
-  // Ex: Solve for ambersands and 'and'
+  // Ex: Solve for ampersands and 'and'
   return artistName
 }
 
@@ -63,7 +61,7 @@ async function buildArtistData(artist) {
   spotifyApi.setAccessToken(data.body['access_token'])
   // Get artist data mainly for their ID
   const artistData = await spotifyApi.searchArtists(artist)
-  // TODO These can return undefined. Add a saftey check
+  // TODO These can return undefined. Add a safety check
   try {
     const artistID = artistData.body.artists.items[0].id
     const tracksData = await spotifyApi.getArtistTopTracks(artistID, 'GB')
